5263dc699146e1b8ed81e58972e878b4fbbc1e51
nathanielpierson <nathanielepierson@gmail.com>
2025-05-12 16:27:55 -0400

    add full functionality for avg_time

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -15,7 +15,7 @@ class LevelsController < ApplicationController
 
       # we are making an array of hashes where each hash has the minute, second, and millisecond data of each run. Note: run_number is the number in the array, not the id)
 
-      minutes_array.push(run_number: x, run_minutes: @level.runs[x].minutes, run_seconds: @level.runs[x].seconds, run_milliseconds: @level.runs[x].milliseconds)
+      minutes_array.push(run_id: @level.runs[x].id, run_minutes: @level.runs[x].minutes, run_seconds: @level.runs[x].seconds, run_milliseconds: @level.runs[x].milliseconds)
       x += 1
     end
     # sorting the array so item [0] is the fastest time
@@ -29,22 +29,26 @@ class LevelsController < ApplicationController
       avg_time_in_secs += (60 * array_sorted[n][:run_minutes])
       p "after minutes added"
       p avg_time_in_secs
-      avg_time_in_secs += (array_sorted[n][:run_minutes])
+      avg_time_in_secs += (array_sorted[n][:run_seconds])
       p "after seconds added"
       p avg_time_in_secs
       avg_time_in_secs += (array_sorted[n][:run_milliseconds] / 100)
       p "after millis added"
       p avg_time_in_secs
     end
-    p avg_time_in_secs
     avg_time_in_secs = avg_time_in_secs / array_sorted.length
-    p avg_time_in_secs
+    avg_time_min_calc = (avg_time_in_secs / 60).floor
+    avg_time_sec_calc = (avg_time_in_secs % 60).round(2)
     @level.update(
       name: @level.name,
       category: @level.category,
+      fastest_time_id: array_sorted[0][:id],
       fastest_time_min: array_sorted[0][:run_minutes],
       fastest_time_sec: array_sorted[0][:run_seconds],
-      fastest_time_milli: array_sorted[0][:run_milliseconds]
+      fastest_time_milli: array_sorted[0][:run_milliseconds],
+
+      avg_time_min: avg_time_min_calc,
+      avg_time_sec: avg_time_sec_calc
     )
     render :show
   end
