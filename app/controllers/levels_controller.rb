bb78a0520b5769bd9e33701333ad3d76b4f1b3b0
nathanielpierson <nathanielepierson@gmail.com>
2025-05-12 16:03:56 -0400

    add logic for calculating avg_time

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -20,10 +20,25 @@ class LevelsController < ApplicationController
     end
     # sorting the array so item [0] is the fastest time
     array_sorted = minutes_array.sort_by { |hsh| [ hsh[:run_minutes], hsh[:run_seconds], hsh[:run_milliseconds] ] }
-    p array_sorted
-    p array_sorted[0][:run_minutes]
-    p array_sorted[0][:run_seconds]
-    p array_sorted[0][:run_milliseconds]
+    # p array_sorted
+    # p array_sorted[0][:run_minutes]
+    # p array_sorted[0][:run_seconds]
+    # p array_sorted[0][:run_milliseconds]
+    avg_time_in_secs = 0.00
+    array_sorted.length.times do |n|
+      avg_time_in_secs += (60 * array_sorted[n][:run_minutes])
+      p "after minutes added"
+      p avg_time_in_secs
+      avg_time_in_secs += (array_sorted[n][:run_minutes])
+      p "after seconds added"
+      p avg_time_in_secs
+      avg_time_in_secs += (array_sorted[n][:run_milliseconds] / 100)
+      p "after millis added"
+      p avg_time_in_secs
+    end
+    p avg_time_in_secs
+    avg_time_in_secs = avg_time_in_secs / array_sorted.length
+    p avg_time_in_secs
     @level.update(
       name: @level.name,
       category: @level.category,
