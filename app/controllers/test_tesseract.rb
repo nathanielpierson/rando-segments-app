f620107866a5786aa5d9ce6073f70173478bb015
nathanielpierson <nathanielepierson@gmail.com>
2026-04-24 18:38:24 -0400

    add new OCR test images

--- app/controllers/test_tesseract.rb
+++ app/controllers/test_tesseract.rb
@@ -4,6 +4,8 @@ require_relative "../../lib/rtesseract_executable"
 
 RTesseract.configure { |c| c.command = RtesseractExecutable.path }
 
-image_path = File.expand_path("../assets/tesseract-test-images/footage0561.tif", __dir__)
-image = RTesseract.new(image_path)
-puts image
+image_path = File.expand_path("../assets/tesseract-test-images/footage0561-crop.tif", __dir__)
+compound = RTesseract.new(image_path)
+image_text = compound.to_s
+p image_text
+# p "nis"
