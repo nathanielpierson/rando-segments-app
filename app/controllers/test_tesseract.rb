b4469f72ea639d1e75d3cd55290c1b2849b35aed
nathanielpierson <nathanielepierson@gmail.com>
2026-04-25 13:04:59 -0400

    add logic for sorting through incrementing list of frames

--- app/controllers/test_tesseract.rb
+++ app/controllers/test_tesseract.rb
@@ -3,9 +3,13 @@ require "rtesseract"
 require_relative "../../lib/rtesseract_executable"
 
 RTesseract.configure { |c| c.command = RtesseractExecutable.path }
-
-image_path = File.expand_path("../assets/tesseract-test-images/footage0561-crop.tif", __dir__)
-compound = RTesseract.new(image_path)
-image_text = compound.to_s
-p image_text
-# p "nis"
+i = 0
+while i < 300
+  image_path = File.expand_path("../assets/tesseract-test-images/rando-test-images/zoom-in#{format('%04d', i)}.tif", __dir__)
+  compound = RTesseract.new(image_path)
+  image_text = compound.to_s
+  if image_text != ""
+    p "#{image_text} #{File.basename(image_path)}"
+  end
+  i += 1
+end
