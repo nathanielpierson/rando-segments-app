1913115932832505fff761027b83d4617d6e0fdb
nathanielpierson <nathanielepierson@gmail.com>
2026-04-30 14:16:37 -0400

    add keyword arrays to test_tesseract for detecting levels

--- app/controllers/test_tesseract.rb
+++ app/controllers/test_tesseract.rb
@@ -5,7 +5,21 @@ require "rtesseract"
 RTesseract.configure { |c| c.command = RtesseractExecutable.path }
 i = 0
 
-keywords = [ "HOMES", "FORTRESS", "GIHOMP", "GIROMP", "CUHOMP" ]
+battlefield_keywords = [ "BOB-OE", "BO-OE", "BOB-OME", "BATTER", "IELD", "BATTERIE", "BATTLEFIELD" ]
+whomps_keywords = [ "HOMES", "FORTRESS", "GIHOMP", "GIROMP", "CUHOMP" ]
+jolly_roger_keywords = [ "JOO", "ROGER", "Bay", "Bat" ]
+cool_keywords = [ "COOL", "MOTALY", "COE", "CODE" ]
+haunt_keywords = [ "BOOS", "HAUT", "HAUNT" ]
+lethal_keywords = [ "LETHAL", "CETHAL", "CETHAC", "Cail" ]
+sand_keywords = [ "SHIFTING", "SAME" ]
+hazy_keywords = [ "Haey", "MEE", "Cave", "Hae" ]
+docks_keywords = [ "DIRE", "DOORS", "GRE", "HIRE", "DOCKS" ]
+snowman_keywords = [ "SHMOOMANS", "SAIOGIANS", "SOOM" ]
+wet_dry_keywords = [ "WET-DRY", "WET", "WET-ORY" ]
+tall_keywords = [ "TAG" "TAC", "Tate", "TaGL", "Taot", "TaGt" ]
+tiny_huge_keywords = [ "TTY", "TIM", "TUM", "-HuE", "-AUBE", "THAW" ]
+rainbow_keywords = [ "RATMEOO", "RIDE", "RATMBOR", "RATMIBOR", "REDE", "RATIO" "RAT BO", "RATE", "RARNBOG" ]
+clock_keywords = [ "TICE", "TOGE", "TICK", "TAG", "TAC", "Go0ce" ]
 
 def contains_any?(str, words)
   words.any? { |word| str.include?(word) }
@@ -26,8 +40,8 @@ while i < 16570
     if image_text != ""
       p "image text: #{image_text} file name: #{File.basename(image_path)} time: #{minutes}:#{seconds}"
       if
-        contains_any?(image_text, keywords)
-        p "We're in Whomp's Fortress rn"
+        contains_any?(image_text, battlefield_keywords)
+        p "level: Bob-omb Battlefield, time: #{minutes}:#{seconds}"
       end
     end
 
